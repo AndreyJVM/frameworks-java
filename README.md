96bb9ff0b4a36597ca2c2ae82d5ca9897234b42a
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-12-02 03:33:14 -0800

    Add new class ./junit5/MyParameterCondition.java

--- README.md
+++ README.md
@@ -12,4 +12,3 @@
 ---
 ## Contacts:
 <a href="mailto:Andrey.Vorobev.AQA@gmail.com" title="Email"><img alt="Email" src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" height="28" align="center"/></a>
-<a href="https://t.me/andreyjqa" title="Email"><img alt="Telegram" src="https://img.shields.io/badge/Telegram-blue?style=for-the-badge&logo=telegram&logoColor=white" height="28" align="center"/></a>
---
+++ junit5/src/test/java/DI/MyParameterResolver.java
@@ -0,0 +1,26 @@
+package DI;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+/** In this example, we can see that this resolver will simply inject hard-coded String parameters
+ * with the value {@code my parameter}*/
+
+public class MyParameterResolver implements ParameterResolver {
+
+    @Override
+    public boolean supportsParameter (ParameterContext parameterContext,
+                                      ExtensionContext extensionContext)
+            throws ParameterResolutionException {
+        return true;
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext,
+                                   ExtensionContext extensionContext)
+            throws ParameterResolutionException {
+        return null;
+    }
+}
